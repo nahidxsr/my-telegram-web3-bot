11605a292e1f62c7d3830384a58369a63d0b37be
NaHid <nahid.xsr@gmail.com>
2025-03-10 12:04:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
-const bot = new Telegraf(process.env.7625765343:AAHT3yUTUtmKtSOv-EDhIYj5fhLynw-Icas);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome! I am your Web3 Bot.'));
 bot.help((ctx) => ctx.reply('Send /start to get started!'));
